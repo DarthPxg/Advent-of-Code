f948f167658914505d08177d00cc93fa10d00f30
DarthPxg <loulougreasley7@gmail.com>
2025-12-18 23:52:42 +0000

    Updated variable names in Day2/Part1/main.py

--- Day2/Part1/main.py
+++ Day2/Part1/main.py
@@ -1,16 +1,16 @@
 ids = open("../input.txt").readline().strip().split(",")
 invalidIds = []
 
-for id in ids:
-  bounds = id.split("-")
-  for n in range(int(bounds[0]), int(bounds[1])+1):
-    n = str(n)
-    l = len(n)
+for id_range in ids:
+  bounds = id_range.split("-")
+  for id in range(int(bounds[0]), int(bounds[1])+1):
+    id = str(id)
+    l = len(id)
 
     if l % 2 != 0:
       continue
 
-    if n[:l//2] == n[l//2:]:
-      invalidIds.append(int(n))
+    if id[:l//2] == id[l//2:]:
+      invalidIds.append(int(id))
 
 print(sum(invalidIds))
