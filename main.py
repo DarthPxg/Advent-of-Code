83ab6f8ce311811e6d13cdac165528d404c78e78
DarthPxg <loulougreasley7@gmail.com>
2025-12-18 22:51:51 +0000

    Part 2 updated

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
-f = open("input.txt")
-
-lines = f.readlines()
+lines = [int(line.strip().replace("L", "-").replace("R", "")) for line in open("input.txt")]
 
 dialNumber = 50
 zeroCount = 0
 
 for line in lines:
-  if len(line) < 2:
-    continue
-  number = int(line[1:])
+  if line < 0:
+    div, mod = divmod(line, -100)
+    zeroCount += div
 
-  if line[0] == "R":
-    dialNumber += number
+    if dialNumber != 0 and dialNumber + mod <= 0:
+      zeroCount += 1
   else:
-    dialNumber -= number
+    div, mod = divmod(line, 100)
+    zeroCount += div
+    if dialNumber + mod >= 100:
+      zeroCount += 1
 
-  dialNumber %= 100
+  dialNumber = (dialNumber + line) % 100
 
-  if dialNumber == 0:
-    zeroCount += 1
-
-print(f"-----{zeroCount}-----")
+print(f"----- {zeroCount} -----")
