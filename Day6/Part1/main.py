3d9c368c8d55a9d904a9f04f94d8ecdeb282ad25
DarthPxg <loulougreasley7@gmail.com>
2025-12-19 21:38:15 +0000

    Completed Day 6 Part 1

---
+++ Day6/Part1/main.py
@@ -0,0 +1,24 @@
+import math
+
+numbers = []
+
+with open("../input.txt") as f:
+  numbers = [line.strip().split() for line in f.readlines()]
+
+total = 0
+
+operations = numbers[-1]
+numbers.pop(-1)
+
+for j in range(len(operations)):
+  numsToAdd = []
+  for i in range(len(numbers)):
+    if 0 <= j < len(numbers[i]) and numbers[i][j] != "" and numbers[i][j] != " ":
+      numsToAdd.append(int(numbers[i][j]))
+
+  if operations[j] == "*":
+    total += math.prod(numsToAdd)
+  else:
+    total += sum(numsToAdd)
+
+print(total)
